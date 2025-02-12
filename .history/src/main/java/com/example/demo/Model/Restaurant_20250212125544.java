e49ba237c0faf82087e9dd1cce2611ba60c76c14
Praharsha-25 <147964310+Praharsha-25@users.noreply.github.com>
2025-02-12 12:59:25 +0530

    first-commit

---
+++ .history/src/main/java/com/example/demo/Model/Restaurant_20250212125544.java
@@ -0,0 +1,107 @@
+package com.example.demo.Model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "restaurants")
+public class Restaurant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "res_id")
+    private String resId;
+
+    @Column(name = "apikey")
+    private String apikey;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "url")
+    private String url;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "locality")
+    private String locality;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "city_id")
+    private String cityId;
+
+    @Column(name = "latitude")
+    private Double latitude;
+
+    @Column(name = "longitude")
+    private Double longitude;
+
+    @Column(name = "zipcode")
+    private String zipcode;
+
+    @Column(name = "country_id")
+    private String countryId;
+
+    @Column(name = "locality_verbose")
+    private String localityVerbose;
+
+    @Column(name = "switch_to_order_menu")
+    private String switchToOrderMenu;
+
+    @Column(name = "cuisines")
+    private String cuisines;
+
+    @Column(name = "average_cost_for_two")
+    private Double averageCostForTwo;
+
+    @Column(name = "price_range")
+    private Integer priceRange;
+
+    @Column(name = "currency")
+    private String currency;
+
+    @Column(name = "thumb")
+    private String thumb;
+
+    @Column(name = "aggregate_rating")
+    private String aggregateRating;
+
+    @Column(name = "rating_text")
+    private String ratingText;
+
+    @Column(name = "rating_color")
+    private String ratingColor;
+
+    @Column(name = "votes")
+    private String votes;
+
+    @Column(name = "photos_url")
+    private String photosUrl;
+
+    @Column(name = "menu_url")
+    private String menuUrl;
+
+    @Column(name = "featured_image")
+    private String featuredImage;
+
+    @Column(name = "has_online_delivery")
+    private String hasOnlineDelivery;
+
+    @Column(name = "is_delivering_now")
+    private String isDeliveringNow;
+
+    @Column(name = "deeplink")
+    private String deeplink;
+
+    @Column(name = "has_table_booking")
+    private String hasTableBooking;
+
+    @Column(name = "events_url")
+    private String eventsUrl;
+}
